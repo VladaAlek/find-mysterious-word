796b6f49a60153bc4ff4e02ddc36ff7014fd36f1
VladaAlek <vladimiraleksic.nis@gmail.com>
2023-12-06 18:36:16 +0000

    add title of the game

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Write your code to expect a terminal of 80 characters wide
+# and 24 rows high
 # stage initial commit
 import sys
 # import exit system function to be used the user chose to exit the game
+
 
 def main():
     """
     Run all program functions
     """
     # add functions as values for the variables
-    #data = get_sales_data()
+    # data = get_sales_data()
     
 
-
+# print the title of the game in capital letters
+text = "Find a mysterious word in five steps"
+print(text.upper())
 
 main()
 
