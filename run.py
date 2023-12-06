5409b45db46f7ae747db7131f8a71213640d7830
VladaAlek <vladimiraleksic.nis@gmail.com>
2023-12-06 18:17:12 +0000

    feat: import exit system function

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-#stage initial commit
+# stage initial commit
+import sys
+# import exit system function to be used the user chose to exit the game
+# sys.exit()
