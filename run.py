45d5359d61c02b3353f3714e662ef90f3a5fae2b
VladaAlek <vladimiraleksic.nis@gmail.com>
2023-12-07 17:19:03 +0000

    add Title and display_welcome_messages

--- run.py
+++ run.py
@@ -2,22 +2,39 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide
 # and 24 rows high
-# stage initial commit
+
 import sys
-# import exit system function to be used the user chose to exit the game
+""" 
+import exit system function to be used the user chose to exit the game
+
+"""
+
+
+def display_welcome_messages():
+    """
+    Welcoming message function
+    """
+    print("Welcome to our guessing the mysterious word game!")
+    print("Follow 5 clues by filling up the input fields one after another")
+    print("to find out the mysterious term.")
+    print("Don't worry if you don't guess from the first attempt;")
+    print("you will get more signs to guide you to the victory.")
 
 
 def main():
     """
     Run all program functions
     """
-    # add functions as values for the variables
-    # data = get_sales_data()
+    display_welcome_messages()
+    
     
 
 # print the title of the game in capital letters
 text = "Find a mysterious word in five steps"
 print(text.upper())
+print("")
+print("")
+print("")
 
 main()
 
